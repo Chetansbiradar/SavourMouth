60c3a4e1927d4f4925acb74c707441c5964798f4
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-06 22:40:48 +0530

    generating elements using Array()

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,8 +1,10 @@
 const Shimmer = () => {
-    let shimmerCards = [];
-    for(let i=0;i<10;i++) {
-        shimmerCards.push(
-        <div className="cardShimmer" key={i}>
+    return (
+        <>
+        <div style={{marginTop:22}}></div>
+        <div className="cards">
+        {Array(10).fill("").map((item, index) => (
+            <div className="cardShimmer" key={index}>
                 <div className="shimmerBG media"></div>
                 <div className="p-32"></div>
                 <div className="shimmerBG title-line"></div>
@@ -11,17 +13,11 @@ const Shimmer = () => {
                 <div className="shimmerBG content-line m-t-24"></div>
                 <div className="shimmerBG content-line"></div>
                 <div className="shimmerBG content-line"></div>
-                <div className="shimmerBG content-line"></div>
                 <div className="shimmerBG content-line end"></div>
             </div>
-        );
-    }
-    return (
-        <div className="cards">
-            {
-                shimmerCards.map((card) => card)
-            }
+        ))}
         </div>
+    </>
     );
 }
 
