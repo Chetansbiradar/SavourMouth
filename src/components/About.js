d6f464a21ea5bc060bd3ce7700f8c2629787c07f
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-06 22:40:19 +0530

    new components

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () =>{
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>I ain't that great, but I appriciate greatness</p>
+        </div>
+    )
+}
+
+export default About;
