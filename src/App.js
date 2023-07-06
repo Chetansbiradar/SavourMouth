2bcc40bacb59a69534bd7e8aa99417af10786240
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-06 22:39:28 +0530

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,48 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-
-
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
-    return(
+    return (
         <>
-            <Header/>
-            <Body/>
-            <Footer/>
+            <Header />
+            <Outlet />
+            <Footer />
         </>
     )
 }
 
+const BrowserRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        errorElement: <Error />,
+        children: [
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/",
+                element: <Body />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />,
+            },
+            {
+                path: "/restaurant/:id",
+                element: <RestaurantMenu />,
+            }
+        ]
+    }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={BrowserRouter}/>);
