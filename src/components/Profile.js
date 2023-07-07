4de2df59e1de5c992a6b0cf81505e58e360de1a6
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-07 23:42:43 +0530

    added cleanup in functional comp

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 const Profile = () => {
+    useEffect(() => {
+        console.log("useEffect");
+        return () => {
+            // cleanup
+            // clear the interval
+            // clear the timeout
+            // remove event listeners
+
+            console.log("useEffect cleanup");
+        }
+    });
+    console.log("Functional render");
     return (
         <div>
             <h1>Profile Functional</h1>
