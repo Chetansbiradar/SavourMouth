8964c959fdcd7df07709e7028d7de6a138498cd2
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-07 20:01:27 +0530

    Added Profile and ProfileClass

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
+import { Outlet } from "react-router-dom";
+import ProfileClass from "./ProfileClass";
+import Profile from "./Profile";
+
 const About = () =>{
     return (
         <div>
             <h1>About Us</h1>
             <p>I ain't that great, but I appriciate greatness</p>
+            <ProfileClass name="Chetan"/>
+            <Profile/>
         </div>
     )
 }
