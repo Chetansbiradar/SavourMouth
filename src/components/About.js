ad6bb6592e211cb69e1fb3928844331e9a95d5dd
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-07 23:42:23 +0530

    Added ClassBased components

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,74 @@
 import { Outlet } from "react-router-dom";
 import ProfileClass from "./ProfileClass";
 import Profile from "./Profile";
+import React from "react";
 
-const About = () =>{
-    return (
-        <div>
-            <h1>About Us</h1>
-            <p>I ain't that great, but I appriciate greatness</p>
-            <ProfileClass name="Chetan"/>
-            <Profile/>
-        </div>
-    )
+// const About = () =>{
+//     return (
+//         <div>
+//             <h1>About Us</h1>
+//             <p>I ain't that great, but I appriciate greatness</p>
+//             <ProfileClass name="Chetan"/>
+//             <Profile/>
+//         </div>
+//     )
+// }
+
+class About extends React.Component {
+    constructor(props){
+        super(props);
+        console.log("Parent About constructor");
+    }
+
+    componentDidMount(){
+        console.log("Parent About componentDidMount");
+    }
+
+    componentDidUpdate(){
+        console.log("Parent About componentDidUpdate");
+    }
+
+    // componentWillUnmount(){
+    //     console.log("Parent About componentWillUnmount");
+    // }
+
+    render(){
+        console.log("Parent About render");
+        return (
+            <div>
+                <h1>About Us</h1>
+                <p>I ain't that great, but I appriciate greatness</p>
+                {<ProfileClass name="Chetan"/>}
+                {/* {<ProfileClass name="Megha"/>} */}
+                {/* {this.state.showProfile && <Profile/>} */}
+                {/* <Outlet /> */}
+            </div>
+        )
+    }
 }
 
 export default About;
+
+/**
+ * Parent Constructor
+ * Parent Render
+ *   Child1 Constructor
+ *   Child1 Render
+ *   Child2 Constructor
+ *   Child2 Render
+ *   Child1 componentDidMount
+ *   Child2 componentDidMount
+ * Parent componentDidMount
+ * 
+ */
+
+/**
+ * Parent Constructor
+ * Parent Render
+ *  Child1 Constructor
+ *  Child1 Render
+ * Parent componentDidMount
+ *  Child1 componentDidMount
+ *  child1 render
+ *  child1 componentDidUpdate
+ */
--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -5,19 +5,66 @@ class Profile extends React.Component {
         super(props);
         this.state = {
             count:0,
-            count2:0
+            count2:0,
+            userInfo:{
+                name: "Chetan",
+                location: "Bangalore"
+            }
         }
+        console.log("Profile constructor");
+    }
+
+    async componentDidMount(){
+        //here we can make api calls bcz after render this method will be called
+        const data = await fetch("https://api.github.com/users/chetansbiradar")
+        const response = await data.json();
+        this.setState({userInfo: response})
+        // this.timer = setInterval(() => {
+        //     console.log("setInterval");
+        // }, 1000);
+        console.log("Profile Child componentDidMount");
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        //this method will be called after every render of the component
+        console.log("Profile Child componentDidUpdate");
+        // if(prevState.count !== this.state.count){
+        //     // Code Here
+        // }
+    }
+
+    componentWillUnmount(){
+        //this method will be called before component is removed from the dom
+        console.log("Profile Child componentWillUnmount");
+        //clean up things like
+        //clearInterval
+        clearInterval(this.timer);
+        //clearTimeout
+        //remove event listeners
     }
 
     render(){ 
+        console.log("Profile Child render");
         return ( 
-        <>
+        <div 
+        style={
+            {
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                width:200,
+                margin: "10 10",
+            }
+        }>
         <h1>Profile Class</h1>
         <p>Name Props {this.props.name}</p>
         <p>Count State {this.state.count}</p>
         <p>Count2 State {this.state.count2}</p>
+        <img src={this.state.userInfo.avatar_url}/>
+        <p>Name GitHub {this.state.userInfo.name}</p>
+        <p>Location GitHub {this.state.userInfo.location}</p>
         <button onClick={() => this.setState({count: ++this.state.count, count2: this.state.count2+1})} >Increment Count</button>
-        </>);
+        </div>);
     }
 }
 
