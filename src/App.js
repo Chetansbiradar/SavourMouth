3c61116e0a972a1f1684c774d317011b39c3819d
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-08 19:46:17 +0530

    added On Demand Loading {lazy,Suspense}

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import Profile from "./components/Profile";
 import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
+const About = lazy(() => import("./components/About"));
+// ^^^^^^^^^^^^^^^^^^
+// This is a dynamic import. It will load the component only when it is required.
+// It is also called code splitting.
+// It is also called Chunking.
+// It is also called lazy loading.
+// It is also called Dynamic import/loading.
+// It is also called On demand loading.
+
 
 const AppLayout = () => {
     return (
@@ -28,7 +37,9 @@ const BrowserRouter = createBrowserRouter([
         children: [
             {
                 path: "about",
-                element: <About />,
+                element: <Suspense fallback={<h1>Loading....</h1>}>
+                    <About />
+                </Suspense>,
                 children: [
                     {
                         path: "profile",
