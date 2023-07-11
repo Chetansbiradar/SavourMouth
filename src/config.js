fc2eaa1b21c5ddbec99d2d73b21de13deb44ddac
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-11 14:26:02 +0530

    added food item url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const IMG_CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+export const FOOD_CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 export const RESTAURANT_MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
