ee8ded0bc1bd7768e4f240b03b9cb14b5338acd5
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-11 14:24:39 +0530

    added cart with ring animation tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,12 @@ import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import UserContext from '../utils/UserContext';
 import { useSelector } from 'react-redux';
-import store from '../utils/store';
 
 const Title = () => (
     <Link to="/">
         <img 
         className="
-        h-24 p-2"
+        h-16"
         src={Logo} 
         alt="logo"
          />
@@ -20,7 +19,7 @@ const Header = () =>{
     const {user} = useContext(UserContext);
     const cartItems = useSelector(store => store.cart.items);
         return (
-            <div className="flex justify-between px-10 shadow-md mb-2">
+            <div className="flex justify-between px-10 shadow-md sticky top-0 bg-white">
                 <Title />
                 <div className="flex">
                     <ul className="flex space-x-5 items-center">
@@ -37,7 +36,17 @@ const Header = () =>{
                             <p>{user.name}</p>
                         </li>
                         <li>
-                            <p>Cart - {cartItems.length}</p>
+                            <Link to="/cart">
+                            <span className="relative inline-flex">
+                                <button type="button" className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md transition ease-in-out duration-150 ring-1">
+                                Cart {cartItems.length}
+                                </button>
+                                <span className="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
+                                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                                <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                                </span>
+                            </span>
+                            </Link>
                         </li>
                     </ul>
                 </div>
